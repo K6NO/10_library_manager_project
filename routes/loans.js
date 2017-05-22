d42af6f7e8cc623ec97f6cdb3b5e73ba30814023
K6NO <tamas.kenessey@gmail.com>
2017-05-22 15:57:45 +0200

    start implementing nested loans object

--- routes/loans.js
+++ routes/loans.js
@@ -1,18 +1,48 @@
 var express = require('express');
 var router = express.Router();
 var Loan = require('../models/index').loans;
+var Book = require('../models/index').books;
+var Patron = require('../models/index').patrons;
 
 
-router.get('/', function(req, res, next) {
-    Loan.findAll().then(function (loans) {
+router.get('/', function(req, res) {
+    Loan.findAll({
+            include : [
+                {model : db.books},
+                {model : db.patrons}
+            ]
+    }).then(function (loans) {
         res.render('all_loans', { loans : loans });
     }).catch(function (err) {
         res.send(500);
     })
 });
 
-router.get('/new', function(req, res, next) {
-    res.render('new_loan', { loan : Loan.build });
+router.get('/new', function(req, res) {
+    var booksAndPatrons = [];
+    Book.findAll()
+        .then(function (books) {
+        booksAndPatrons.push(books);
+    });
+    Patron.findAll()
+        .then(function (patrons) {
+            //console.log(patrons);
+            booksAndPatrons.push(patrons);
+        })
+        .then(function () {
+            //console.log('books:_____');
+            //console.log(booksAndPatrons[0]);
+            //console.log('patrons:_____');
+            console.log(booksAndPatrons[1]);
+            res.render('new_loan', {
+                loan : Loan.build(),
+                books : booksAndPatrons[0],
+                patrons : booksAndPatrons[1]
+            });
+        })
+        .catch(function (err) {
+            res.send(500);
+        })
 });
 
 router.post('/new', function (req, res, next) {
