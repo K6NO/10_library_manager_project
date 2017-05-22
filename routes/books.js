5dea20211132dde6ad46bfd794f546cb2c25d444
K6NO <tamas.kenessey@gmail.com>
2017-05-22 12:47:55 +0200

    updating patrons works

--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Book = require('../models').books;
 
-
+// GET all books
 router.get('/', function(req, res, next) {
     Book.findAll().then(function (books) {
         res.render('all_books', { books : books});
@@ -11,10 +11,58 @@ router.get('/', function(req, res, next) {
     })
 });
 
+//GET single book
+router.get('/book_detail/:id', function (req, res, next) {
+    Book.findById(req.params.id).then(function (book) {
+        if(book){
+            res.render('book_detail', {
+                book : book
+            })
+        } else {
+            res.send(404);
+        }
+    }).catch(function (err) {
+        res.send(500);
+    })
+});
+
+// UPDATE book details - router.put resultes in 404 error
+router.post('/book_detail/:id', function (req, res, next) {
+    console.log(req.params.id);
+    Book.findById(req.params.id)
+        .then(function (book) {
+            console.log(book);
+        if (book){
+            return book.update(req.body);
+        } else {
+            res.send(404);
+        }
+    })
+        .then(function (book) {
+            res.redirect('/books/book_detail/' + book.id);
+        })
+        .catch(function (err) {
+            if(err.name === 'SequelizeValidationError'){
+                var book = Book.build(req.body);
+                book.id = req.params.id;
+                res.render('/book_detail/' + book.id, {
+                    book : book,
+                    errors : err.errors
+                })
+            } else {
+                throw err;
+            }
+    }).catch(function (err) {
+        res.send(500);
+    })
+});
+
+// new book
 router.get('/new', function (req, res, next) {
     res.render('new_book', { book : Book.build()})
 });
 
+// POST save new book
 router.post('/new', function (req, res, next) {
     console.log(req.body);
     Book.create(req.body)
@@ -35,6 +83,4 @@ router.post('/new', function (req, res, next) {
             res.send(500);
         })
 });
-
-
 module.exports = router;
--- routes/patrons.js
+++ routes/patrons.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Patron = require('../models').patrons;
 
+// GET all patrons
 router.get('/', function(req, res, next) {
     Patron.findAll().then(function (patrons) {
         res.render('all_patrons', {patrons: patrons });
@@ -11,10 +12,12 @@ router.get('/', function(req, res, next) {
     })
 });
 
+// GET new patron page
 router.get('/new', function(req, res, next) {
     res.render('new_patron', { patron: Patron.build()});
 });
 
+// POST new patron
 router.post('/new', function(req, res, next) {
     console.log(req.body);
     Patron.create(req.body)
@@ -34,6 +37,52 @@ router.post('/new', function(req, res, next) {
         .catch(function (err) {
             res.send(500);
         });
+});
+
+//GET single patron
+router.get('/patron_detail/:id', function (req, res, next) {
+    Patron.findById(req.params.id).then(function (patron) {
+        if(patron){
+            res.render('patron_detail', {
+                patron : patron
+            })
+        } else {
+            res.send(404);
+        }
+    }).catch(function (err) {
+        res.send(500);
+    })
+});
+
+// UPDATE patron
+router.post('/patron_detail/:id', function (req, res, next) {
+    console.log(req.params.id);
+    Patron.findById(req.params.id)
+        .then(function (patron) {
+            console.log(patron);
+            if (patron){
+                return patron.update(req.body);
+            } else {
+                res.send(404);
+            }
+        })
+        .then(function (patron) {
+            res.redirect('/patrons/patron_detail/' + patron.id);
+        })
+        .catch(function (err) {
+            if(err.name === 'SequelizeValidationError'){
+                var patron = Patron.build(req.body);
+                patron.id = req.params.id;
+                res.render('/patron_detail/' + patron.id, {
+                    patron : patron,
+                    errors : err.errors
+                })
+            } else {
+                throw err;
+            }
+        }).catch(function (err) {
+        res.send(500);
+    })
 });
 
 module.exports = router;
